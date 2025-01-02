5f8913f0e37096115b0a9594f3c1eb66452e0b91
KimximyaFan <107273680+KimximyaFan@users.noreply.github.com>
2025-01-02 09:37:52 +0900

    add

---
+++ C_17213.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class C_17213 {
+    public static void main (String[] args) throws IOException{
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+    }
+}
