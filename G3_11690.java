7ce98aabb6db67351b566ebb5348d70e34052810
KimximyaFan <107273680+KimximyaFan@users.noreply.github.com>
2025-01-23 13:00:20 +0900

    add:

---
+++ G3_11690.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class G3_11690
+{
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        long N = Integer.parseInt(input.readLine());
+
+        ArrayList<Long> prime = Sieve_Of_Eratosthenes((int)N);
+
+        long divider = 4294967296L;
+        long ans = 1L;
+
+        for (int i=0; i<prime.size(); i++)
+        {
+            long current_prime = prime.get(i);
+            long current_value = current_prime;
+            
+            while ( true )
+            {
+                if ( current_value * current_prime <= N )
+                    current_value *= current_prime;                 
+                else
+                    break;
+            }
+
+            ans = (ans * current_value) % divider;
+        }
+
+        System.out.print(ans);
+    }
+
+    static ArrayList<Long> Sieve_Of_Eratosthenes (int N)
+    {
+        ArrayList<Long> al = new ArrayList<>();
+        boolean[] check = new boolean[N+1];
+
+        for (int i=2; i*i<=N; i++)
+        {
+            if ( check[i] == false )
+            {
+                for (int j=i*i; j<=N; j+=i)
+                    check[j] = true;
+            }
+        }
+
+        for (int i=2; i<=N; i++)
+        {
+            if ( check[i] == false )
+                al.add((long)i);
+        }
+
+        return al;
+    }
+}
