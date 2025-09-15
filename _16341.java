2e39752e0ce15855df98e88b80da80d9f9ba079d
KimximyaFan <107273680+KimximyaFan@users.noreply.github.com>
2025-09-16 07:33:26 +0900

    add:

---
+++ _16341.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class _16341
+{
+    public static void main (String[] args) throws IOException
+    {
+        System.out.print( (readInt() + readInt())%2 == (readInt() + readInt())%2 ? "black" : "white" );
+    }
+
+    static int readInt() throws IOException
+    {
+        int c, num = 0;
+        boolean neg = false;
+        while ((c = System.in.read()) <= 32);
+        if (c == '-') { neg = true; c = System.in.read(); }
+        do { num = (num << 3) + (num << 1) + (c & 15); } 
+        while ((c = System.in.read()) >= '0' && c <= '9');
+        return neg ? -num : num;
+    }
+}
